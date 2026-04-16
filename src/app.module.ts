dddc802139a01dc46a69caa28c2989114732c0af
LudovicMAK <ludovic93mak@gmail.com>
2026-04-16 21:13:09 +0200

    save

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,8 @@ import { TournamentModule } from './tournament/tournament.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { AuthController } from './auth/auth.controller';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
